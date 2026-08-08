c37f304ee1d65bac230b18db76398c7553c65789
idy534 <46567512+idy534@users.noreply.github.com>
2026-08-08 19:36:36 +0800

    style(renderer): load response action styles

--- desktop/renderer/src/main.tsx
+++ desktop/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./components/response-actions.css";
 
 
 createRoot(document.getElementById("root")!).render(
